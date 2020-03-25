69a2ae14bc1f6a06248778874ab9e1ce4eb0295f
Robin Joseph <robinmjoseph08@gmail.com>
2020-03-24 22:41:19 -0700

    feat(single): add post navigation (#4)

--- scripts/fontawesome.sh
+++ scripts/fontawesome.sh
@@ -1,12 +1,20 @@
 #!/bin/sh -ex
 
-icons="github-square linkedin twitter-square"
+brand_icons="github-square linkedin twitter-square"
+solid_icons="chevron-left chevron-right"
 
 dest=assets/fontawesome
-url=https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/svgs/brands/
+brand_url=https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/svgs/brands/
+solid_url=https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/svgs/solid/
 
 mkdir -p "${dest}"
-for icon in $icons; do
+
+for icon in $brand_icons; do
   icon="${icon}.svg"
-  wget -O "${dest}/${icon}" "${url}/${icon}"
+  wget -O "${dest}/${icon}" "${brand_url}/${icon}"
+done
+
+for icon in $solid_icons; do
+  icon="${icon}.svg"
+  wget -O "${dest}/${icon}" "${solid_url}/${icon}"
 done
